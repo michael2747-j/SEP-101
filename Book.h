4b8dac0a40374a68a894da281611710139614df5
michael2747-j <139519727+michael2747-j@users.noreply.github.com>
2023-11-22 17:49:21 -0500

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,10 +1,22 @@
+///////////////////////////////////////////////////////////////////////////
+// File Name: Libarary Management System
+// Group Number:  4
+// Group Members Names : MICHAEL MELLES (111838223),
+// Group Members Seneca Email : mmellese@myseneca.ca, ssnijjer@myseneca.ca, zahmed79@myseneca.ca, ayush1@myseneca.ca
+// Date : 21/11 2023
+// Authenticity Declaration :
+// We declare this submission is the result of our group work and has not been
+// shared with any other groups/students or 3rd party content provider.This submitted
+// piece of work is entirely of my own creation.
+//////////////////////////////
+
 
 #define BOOK_H
 #include "LibraryConfig.h"
 
 
-struct CommonBookAttributes {
-
+struct CommonBookAttributes {  //Embedded structure of CommonBookAttributes. 
+	                           //Embedding the common attributes ensures we have all the generic book details available within this structure.
 
 	int id;
 	char title[MAX_TITLE_LEN];
@@ -19,7 +31,7 @@ struct CommonBookAttributes {
 
 
 
-struct Book {
+struct Book {  //To represent physical books, which include all common attributes plus specific ones related to their physical nature.
 
 	struct CommonBookAttributes commonAttributes;
 
@@ -31,7 +43,7 @@ struct Book {
 
 };
 
-struct EBook {
+struct EBook {  //To represent eBooks, which do not have a physical presence but have digital access details.
 
 	struct CommonBookAttributes commonAttributes;
 	char accessLink[MAX_LINK_LEN];
@@ -40,8 +52,3 @@ struct EBook {
 };
 
 void initializeBook(struct Book* book);
-
-
-
-
-
